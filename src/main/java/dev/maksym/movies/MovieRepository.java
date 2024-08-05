9c0f857489692a9e90e87981d8bfc6bee71c386b
Maksym <maksyuze456@gmail.com>
2024-08-05 23:48:12 +0200

    First Commit

---
+++ src/main/java/dev/maksym/movies/MovieRepository.java
@@ -0,0 +1,14 @@
+package dev.maksym.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId>{
+    public Optional<Movie> getSingleMovieByImdbId(String imdbId);
+
+}
---
+++ src/main/java/dev/maksym/movies/MovieService.java
@@ -0,0 +1,22 @@
+package dev.maksym.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+    public Optional<Movie> getSingleMovie(String imdbId){
+        return movieRepository.getSingleMovieByImdbId(imdbId);
+    }
+}
